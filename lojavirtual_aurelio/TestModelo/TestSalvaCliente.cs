11a64a5b2242a1bfccb89b579fdc4dc1cde46204
faurelio <faureliodasilva@gmail.com>
2016-03-23 18:54:01 -0300

    Discricao de teste

---
+++ lojavirtual_aurelio/TestModelo/TestSalvaCliente.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Modelo;
+
+namespace TestModelo
+{
+    [TestClass]
+    public class TestSalvarCliente
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void DeveLancarExcecaoSeONomeForNulo()
+
+        {
+            var cliente = new Cliente();
+            new Clientes().Salvar(cliente);
+
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void DeveLancarExcecaoSeONomeForVazio()
+
+        {
+            var cliente = new Cliente
+            {
+                Nome = ""
+            };
+            new Clientes().Salvar(cliente);
+        }
+    }
+}
