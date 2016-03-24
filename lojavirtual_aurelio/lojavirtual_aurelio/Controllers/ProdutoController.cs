0e26c3d662e3b7d120772418687428e7dfd185c1
faurelio <faureliodasilva@gmail.com>
2016-03-23 21:33:45 -0300

    Criacao lista produtos

---
+++ lojavirtual_aurelio/lojavirtual_aurelio/Controllers/ProdutoController.cs
@@ -0,0 +1,33 @@
+﻿using lojavirtual_aurelio.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace lojavirtual_aurelio.Controllers
+{
+    public class ProdutoController : Controller
+    {
+     public ActionResult Lista()
+        {
+            var lista = new List<ProdutoVienModel>
+            {
+                new ProdutoVienModel
+                {
+                    Nome = "Chuteira Nike",
+                    Categoria = "Calcados"
+                    },
+                    new ProdutoVienModel
+                    {
+                        Nome = "Camisa Adidas",
+                        Categoria = "Roupas"                             
+                    }
+            };
+            return View(lista);
+        }   
+
+    }
+}
---
+++ lojavirtual_aurelio/lojavirtual_aurelio/Models/ProdutoVienModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace lojavirtual_aurelio.Models
+{
+    public class ProdutoVienModel
+    {
+        public string Nome { get; set; }
+
+        public string Categoria { get; set; }
+    }
+}
